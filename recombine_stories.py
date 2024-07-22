aed507c4f5960c91cbc8f5e9c52dd8439ce52adc
Rob Neuhaus <rrenaud@gmail.com>
2024-07-22 12:42:44 -0700

    made recombine stories also support flat list of json files

--- recombine_stories.py
+++ recombine_stories.py
@@ -11,6 +11,8 @@ class StoryJsonCombiner:
         self.seen_md5s = set()
     
     def _write_buffered_outputs(self):
+        if not self.buffered_output:
+            return
         print('writing output', self.num_outputs)
         with open(f'spanish_stories/stories_{self.num_outputs:02d}.json', 'w') as fp:
             json.dump(self.buffered_output, fp)
@@ -28,23 +30,44 @@ class StoryJsonCombiner:
             self._write_buffered_outputs()        
 
 
-def main():
-    output_combiner = StoryJsonCombiner()
-    for fn in os.listdir('/home/rrenaud/work/'):
+def tarfile_story_iterator(tarfile_path):
+    with tarfile.open(tarfile_path, 'r:gz') as tar:
+        for member in tar.getmembers():
+            if member.isfile():
+                f = tar.extractfile(member)
+                try:
+                    story = json.load(f)
+                except Exception as e:
+                    print('Error loading json', e, ' in', member.name)
+                    continue
+                yield story
+
+def tarfiles_story_iterator(tarfile_dir):
+    for fn in os.listdir(tarfile_dir):
         if not fn.endswith('.tar.gz'):
             continue
+        for story in tarfile_story_iterator(tarfile_dir + fn):
+            yield story
 
-        print('working on', fn)
-        with tarfile.open('/home/rrenaud/work/' + fn, 'r:gz') as tar:
-            for member in tar.getmembers():
-                if member.isfile():
-                    f = tar.extractfile(member)
-                    try:
-                        story = json.load(f)
-                    except Exception as e:
-                        print('Error loading json', e, ' in', member.name)
-                        continue
-                    output_combiner.add_story(story)
+def raw_json_story_iterator(json_dir):
+    for fn in os.listdir(json_dir):
+        if not fn.endswith('.json'):
+            continue
+        with open(json_dir + '/' + fn) as fp:
+            try:
+                story = json.load(fp)
+            except Exception as e:
+                print('Error loading json', e, ' in', fn)
+                continue
+            yield story
+
+
+def main():
+    output_combiner = StoryJsonCombiner()
+    # story_source = tarfiles_story_iterator('/home/rrenaud/work/')
+    story_source = raw_json_story_iterator('/home/rrenaud/multilingual_tinystories/spanish_stories_translated')
+    for story in story_source:
+        output_combiner.add_story(story)
 
     output_combiner._write_buffered_outputs()
     print('saw', len(output_combiner.seen_md5s), 'unique stories')
